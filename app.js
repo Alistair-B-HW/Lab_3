648174275f4b9bee5e73950d9b7c4db4428cb7f0
Alistair-B-HW <91805078+Alistair-B-HW@users.noreply.github.com>
2021-11-29 03:16:40 +0000

    Completed til Catalog Became stuck after lab sheet become no more help.

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require("express");
 //creating app
 const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
 //send an HTTP response when receiving HTTP GET /
 app.get("/", (req, res) => {
-  res.send("Hello, your server is up and running!!");
+  res.render("index");
 });
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
 });
+
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+
+//route for logging in
+app.get("/login", (req, res) => {
+  res.render("signin");
+});
+
+//route for registering
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+const router = require("./routes/apis.js");
+app.use(router);
+
+router.post("/api/catalog");
